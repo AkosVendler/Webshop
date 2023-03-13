4472f725a2dc2075e7f29b1597901a2a50a67afe
TheShoeBox <73746378+AkosVendler@users.noreply.github.com>
2023-03-13 17:30:32 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function hoverOut(img){
+    img.src = "web1.png"
+}
+function hover(img){
+    img.src = "web1-1.png"
+}
+
+function back(img){
+    img.src = "web2.png"
+}
+function change(img){
+    img.src = "web2-2.png"
+}
